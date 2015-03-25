d4270839c0470cf697e554c61aa6564afccf1641
mbech <matt.b.bechtel@gmail.com>
2015-03-24 20:48:11 -0700

    Fix monk errors, update userlist routes/view

--- nodetest1/app.js
+++ nodetest1/app.js
@@ -5,8 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// Link up MongoDB
+var mongo = require('mongodb')
+var monk = require('monk')
+var db = monk('localhost:27017/nodetest1');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
+
 
 var app = express();
 
@@ -21,6 +27,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Make the db accessible to router
+app.use(function(res, req, next){
+  req.db = db;
+  next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
